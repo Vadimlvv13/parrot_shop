849cc26b0edde1efc3ec5bf3f9be2d10bba2b2cb
Vadim Lisovyi <lisoviy13@gmail.com>
2021-12-08 13:02:20 +0200

    edit config/web

--- config/web.php
+++ config/web.php
@@ -7,11 +7,16 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'ru-RU',
+    'name'=>'Parrot Shop',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '1ZGX5isueQLYP4NxgMONzczzz7QyReoh',
@@ -61,13 +66,13 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1', '10.13.0.9', '192.168.33.143', '192.168.33.180', '192.168.33.0/24', '10.13.0.0/24'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*', '192.168.178.20']
+        'allowedIPs' => ['127.0.0.1', '::1', '10.13.0.9', '192.168.33.143', '192.168.33.180', '192.168.33.0/24', '10.13.0.0/24'],
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
