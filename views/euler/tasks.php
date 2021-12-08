71c48e3fee2a8acc61548f1f48bdf83a387c49a4
Vadim Lisovyi <lisoviy13@gmail.com>
2021-12-08 15:30:32 +0200

    edit euler/tasks

--- views/euler/tasks.php
+++ views/euler/tasks.php
@@ -19,16 +19,34 @@ $this->title = 'Project Euler Tasks List';
             <a href="/web/euler/task1">Задача №1<a>
             <br>
             <p>Числа, кратные 3 или 5.</p>
-            <h5 style="color: green;">Выполнено</h5>
+            <!-- <h5 style="color: green;">Выполнено</h5> -->
         </div>
 
         <div class="jumbotron">
             <a href="/web/euler/task2">Задача №2<a>
             <br>
             <p>Четные числа Фибоначчи.</p>
+            <!-- <h5 style="color: green;">Выполнено</h5> -->
         </div>
-        <p class="lead">Задача №3.</p>
-        <p class="lead">Задача №4.</p>
+        
+        <div class="jumbotron">
+            <a href="/web/euler/task3">Задача №3<a>
+            <br>
+            <p>Наибольший простой делитель.</p>
+            <!-- <h5 style="color: green;">Выполнено</h5> -->
+        </div>
+        <div class="jumbotron">
+            <a href="/web/euler/task4">Задача №4<a>
+            <br>
+            <p>Наибольшее произведение-палиндром.</p>
+            <!-- <h5 style="color: green;">Выполнено</h5> -->
+        </div>
+        <div class="jumbotron">
+            <a href="/web/euler/task5">Задача №5<a>
+            <br>
+            <p>Наименьшее кратное.</p>
+            <!-- <h5 style="color: green;">Выполнено</h5> -->
+        </div>
         
 
     </div>
