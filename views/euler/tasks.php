ef86925d3eec445e49433012229b205b83ac39a6
Vadim Lisovyi <lisoviy13@gmail.com>
2021-12-08 14:53:33 +0200

    edit euler/tasks

--- views/euler/tasks.php
+++ views/euler/tasks.php
@@ -2,25 +2,30 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'Project Euler Task1';
+$this->title = 'Project Euler Tasks List';
 ?>
 <div class="euler-task1">
 
     <div class="jumbotron text-center bg-transparent">
         <h1 class="display-4">Project Euler Task List</h1>
 
-        <p class="lead">Задача №1.</p>
-        <p class="lead">Задача №2.</p>
-        <p class="lead">Задача №3.</p>
 
 
     </div>
 
     <div class="body-content">
-        <a href="/web/euler/task1">Задача №1</a>
-        
-        <p class="lead">Задача №2.</p>
+        <div class="jumbotron">
+            <a href="/web/euler/task1">Задача №1<a>
+            <br>
+            <p>Числа, кратные 3 или 5.</p>
+        </div>
+        <div class="jumbotron">
+            <a href="/web/euler/task2">Задача №2<a>
+            <br>
+            <p>Четные числа Фибоначчи.</p>
+        </div>
         <p class="lead">Задача №3.</p>
+        <p class="lead">Задача №4.</p>
         
 
     </div>
