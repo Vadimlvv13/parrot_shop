c7e1e8be2c0501b8c685a54cd39043010114bb8f
Vadim Lisovyi <lisoviy13@gmail.com>
2021-12-09 09:11:12 +0200

    add euler/task3

---
+++ views/euler/task3.php
@@ -0,0 +1,45 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Project Euler Task3';
+?>
+<div class="euler-task1">
+    <h1 class="display-4", style="text-align: center">Проект Эйлера</h1>
+    <div>
+        <a href="/web/euler/tasks">Back to list</a>
+    </div>
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Задача №3</h1>
+        <h2>Наибольший простой делитель.</h2>
+        <p class="lead">
+
+
+
+
+
+Простые делители числа 13195 - это 5, 7, 13 и 29.
+
+Каков самый большой делитель числа 600851475143, являющийся простым числом?
+
+</p>
+
+    </div>
+
+    <div class="body-content">
+        <?php 
+            //echo '<p>Сумма всех чисел меньше 1000 равна '.$sum.'.</p>';
+            // echo '<pre>';
+            // var_dump($fib);
+            // echo '</pre>';
+            // echo '<br><br>';
+            // echo '<pre>';
+            // var_dump($fib2);
+            // echo '</pre>';
+            // echo '<br><br>';
+            // echo $sum;
+        ?>
+        
+
+    </div>
+</div>
