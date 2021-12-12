1048b5cb9a1ee80f843879f7218668244a3db01e
Vadim Lisovyi <lisoviy13@gmail.com>
2021-12-12 21:35:18 +0200

    edit euler/task4

--- views/euler/task4.php
+++ views/euler/task4.php
@@ -28,12 +28,12 @@ $this->title = 'Project Euler Task4';
 
     <div class="body-content">
         <?php 
-            foreach ($res as $value) {
+            /*foreach ($res as $value) {
                 echo $value[0].' x '.$value[1].' = '.$value[2].'<br>';
-            }
+            }*/
             //echo '<p>Сумма всех чисел меньше 1000 равна '.$sum.'.</p>';
             // echo '<pre>';
-            // var_dump($mas);
+             var_dump($res);
             // echo '</pre>';
             // echo '<br><br>';
             // echo '<pre>';
