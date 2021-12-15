812058d7fc522b6b95309a71847ce8f3a6bef1af
Vadim Lisovyi <lisoviy13@gmail.com>
2021-12-15 17:01:28 +0200

    edit euler/task11

--- views/euler/task011.php
+++ views/euler/task011.php
@@ -111,6 +111,7 @@ $this->title = 'Project Euler Task11';
                 for ($i=0; $i <=20 ; $i++) { 
                     echo $val[$i].'  ';
                 }
+                echo '<br>';
             }
         ?>
         
