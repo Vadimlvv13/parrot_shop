791edd1d1d68b833978be93c05f970bf97642719
Vadim Lisovyi <lisoviy13@gmail.com>
2022-01-10 17:32:53 +0200

    edit euler/task14

--- controllers/EulerController.php
+++ controllers/EulerController.php
@@ -618,15 +618,21 @@ class EulerController extends Controller
 			$num = $i;
 			$count = 1;
 			while ($num !== 1) {
-				/*foreach ($mas as $key => $value) {
-					// code...
-				}*/
-				if ($num % 2 == 0) {
-					$num /= 2;
-				}else{
-					$num = 3 * $num + 1;
+				foreach ($mas as $key => $value) {
+					if ($num == $key) {
+						$count += $value;
+						$num = 1;
+						break;
+					}
 				}
-				$count += 1;
+				if ($num !== 1){
+					if ($num % 2 == 0) {
+						$num /= 2;
+					}else{
+						$num = 3 * $num + 1;
+					}
+					$count += 1;
+				}
 			}
 			$mas[$i] = $count;
 		}
