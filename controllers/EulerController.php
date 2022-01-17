4e5ad69c5d950bf76ab5bc276aca4e38c6772fb8
Vadim Lisovyi <lisoviy13@gmail.com>
2022-01-17 15:53:59 +0200

    edit euler/task16

--- controllers/EulerController.php
+++ controllers/EulerController.php
@@ -681,7 +681,7 @@ class EulerController extends Controller
 	{
 		$num = 2; // число которое возводим в степень.
 		$n = 15; // степень на которую мы возводим число.
-		$res = "$num";
+		$res = (string)$num;
 		/*for ($i=1; $i < $n; $i++) { 
 			
 		}*/
